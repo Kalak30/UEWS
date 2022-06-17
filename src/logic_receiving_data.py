6c093d3ef7d07ed007d7cb219d3ec3d61b321347
jonkopf <jonathan.kopf.ctr@navy.mil>
2022-06-17 08:45:26 -0700

    Moved Calculation functions to a different file.

--- src/logic_receiving_data.py
+++ src/logic_receiving_data.py
@@ -1,5 +1,4 @@
 import datetime
-import math
 import numpy as np
 from shapely.geometry import Point, Polygon
 from multiprocessing.connection import Client
@@ -22,54 +21,16 @@ chords_outer = [(2500, -1400), (2500, 1900), (3100, 1900), (5900, 1600), (8500, 
 inner_poly = Polygon(chords_inner)
 
 
-# needs updating
-def alert_detector(current_position, last_position, current_time, last_time):
-    return 0
-
-
-# get my_speed in yards per second
-def get_speed(my_speed, current_position, last_position, seconds):
-    # alternatively this can be done by using the given my_speed and heading.
-    print(current_position[0])
-    print(last_position[0])
-    # calculated my_speed in each direction with current and last positions (each position is in yards)
-    print("seconds: ", seconds)
-    my_speed[0] = (current_position[0] - last_position[0]) / seconds
-    my_speed[1] = (current_position[1] - last_position[1]) / seconds
-    my_speed[2] = (current_position[2] - last_position[2]) / seconds
-
-
-# calculate speed in x and y direction from the given knots and heading values
-def get_speed_from_knots(given_speed, my_knots, heading):
-    # rotate 90 so that head is degrees off of x-axis (instead of y-axis) ((or y instead of x, not sure))
-    heading = heading - 90
-    # print("incoming knots : ",my_knots,"incoming heading: ",heading)
-    given_speed[0] = (1.68781 * (my_knots * math.sin((math.radians(heading)))))
-    given_speed[1] = (1.68781 * (my_knots * math.cos((math.radians(heading)))))
-
-
-# calculate knots from calculated speed from given positioning
-def get_knots(my_speed):
-    # 1 yard/second = 1.77745 my_knots
-    # totalSpeed = math.sqrt(my_speed[0]**2 + my_speed[1]**2 + my_speed[2]**2)
-    xy_speed = math.sqrt((my_speed[0]) ** 2 + (my_speed[1] ** 2))
-    total_speed = math.sqrt(xy_speed ** 2 + (my_speed[2]) ** 2)
-
-    print("xy_speed:     ", xy_speed)
-    print("total speed: ", total_speed)
-
-    # total_knots = totalSpeed * 0.592484
-    total_knots = xy_speed * 0.592484
-
-    # note: incoming knots only account for xy directions
-    return total_knots
-
-
 # config file variables
 # alertType = 'consecutive'  # or 'cumulative'
 alertType = 'cumulative'
 alertNum = 3
 alertNumTotal = 5  # only used for cumulative (i.e. 3 out of past 5)
+
+
+# needs updating
+def alert_detector(current_position, last_position, current_time, last_time):
+    return 0
 
 
 # def setProjPosition(proj_pos, my_speed):
