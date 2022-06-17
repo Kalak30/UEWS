a2f931172202e265a26e0930249513b2984abd90
jonkopf <jonathan.kopf.ctr@navy.mil>
2022-06-17 10:14:25 -0700

    Moved the parsing of the PP record to its own function as it was complex enough to do so.
    Fixed a logic error in calculating the average of all seen PP values. Using a proper rolling average calculating algorithm.
    
    Created a static decorator to create static function variables

--- src/rsdf_parse.py
+++ src/rsdf_parse.py
@@ -1,6 +1,44 @@
 import datetime
 
 
+# A decorator to create a static function variable
+def static(name, val):
+    def decorate(func):
+        setattr(func, name, val)
+        return func
+    return decorate
+
+
+# Calculates the approximate rolling average given the old average, new value, and the total number of samples
+def approx_rolling_avg(old_avg, new_val, number_of_samples):
+    return old_avg * (number_of_samples - 1) / number_of_samples + new_val / number_of_samples
+
+
+# Creating some static variables to make life easier when calculating rolling average
+@static("seen_pp", False)
+@static("num_pp", 0)
+@static("rolling_avg", list())
+def parse_pp(words):
+    input_position = list(map(float, [words[1], words[2], words[3], words[10], words[11]]))
+
+    # Calculate average PP position if there are multiple
+    if parse_pp.seen_pp:
+
+        if parse_pp.num_pp < 1:
+            print("Have seen a PP record but have not parsed it. Returning value of this PP record")
+            return input_position
+
+        input_position = [approx_rolling_avg(avg_val, ip_val, parse_pp.num_pp) for avg_val, ip_val in
+                          zip(input_position, parse_pp.rolling_avg)]
+    else:
+        print("found pp")
+        seen_pp = True
+
+    parse_pp.num_pp += 1
+    rolling_avg = input_position
+    return input_position
+
+
 # takes entire message and extracts + updates input position and time
 def parse_data(message, input_position, input_time, last_time):
     lines = message.split('\n')
@@ -16,35 +54,21 @@ def parse_data(message, input_position, input_time, last_time):
 
         # if HS line, get the times, then go to next line
         elif words[0] == "HS":
-            for i in range(len(words)):
-                try:
-                    words[i] = int(words[i])
-                except ValueError as verr:
-                    pass
             # store and get the next line
             last_time = input_time
-            input_time = datetime.datetime(words[1], words[2], words[3], words[4], words[5], words[6])
 
+            try:
+                words = [int(word) for word in words]
+            except ValueError as verr:
+                print("Could not convert data to integer")
+
+            input_time = datetime.datetime(words[1], words[2], words[3], words[4], words[5], words[6])
             print("input time", input_time)
             print("last time :", last_time)
             continue
 
-        # if the first instance of PP data, set the input positions
-        elif words[0] == 'PP' and words[4] == '11' and first_pp == 1:
-            last_position = input_position
-            input_position = list(map(float, [words[1], words[2], words[3], words[10], words[11]]))
-            first_pp = 0
-            print("found pp")
-            continue
-
-        # if second or more instance of PP, take average of positions and new PP
-        elif words[0] == 'PP' and words[4] == '11' and first_pp == 0:
-            new_position = list(map(float, [words[1], words[2], words[3], words[10], words[11]]))
-
-            # combine the new position numbers to get theaverage
-            for i in range(len(input_position)):
-                input_position[i] = (input_position[i] + new_position[i]) / 2
-
+        elif words[0] == 'PP' and words[4] == '11':
+            parse_pp(words)
             continue
 
         # if CS line, then it is th end of message
