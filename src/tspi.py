f094903ab563520449ec0ab28fb50faee04a0359
jonkopf <jonathan.kopf.ctr@navy.mil>
2022-06-17 14:03:49 -0700

    Needed totals for x, y, z speed not position

--- src/tspi.py
+++ src/tspi.py
@@ -1,5 +1,6 @@
 import datetime
 from collections import deque
+import tspi_calc
 
 
 class TSPIRecord:
@@ -13,6 +14,10 @@ class TSPIRecord:
         self.x_speed = x_speed
         self.y_speed = y_speed
 
+        # Gets updated upon addition to a TSPIStore object
+        self.d_x = 0
+        self.d_y = 0
+
         # Time in datetime format
         self.time = time
 
@@ -25,20 +30,21 @@ class TSPIStore:
         # Oldest record on right, the newest record on left
         self.records = deque()
         self.record_ttl = ttl
-        self.x_sum = 0
-        self.y_sum = 0
-        self.z_sum = 0
+        self.x_total_speed = 0
+        self.y_total_speed = 0
+        self.z_total_speed = 0
 
     def add_record(self, record: TSPIRecord):
         new_time = record.time
-        self.x_sum += record.x
-        self.y_sum += record.y
-        self.z_sum += record.z
+        tspi_calc.get_speed()
+        self.x_total_speed += record.d_x
+        self.y_total_speed += record.d_y
+        self.z_total_speed += record.d_z
 
         while self.records[-1].is_old(self.record_ttl, new_time):
-            self.x_sum -= self.records[-1].x
-            self.y_sum -= self.records[-1].y
-            self.z_sum -= self.records[-1].z
+            self.x_total_speed -= self.records[-1].d_x
+            self.y_total_speed -= self.records[-1].d_y
+            self.z_total_speed -= self.records[-1].d_z
             self.records.pop()
 
     def get_newest_record(self):
@@ -46,4 +52,4 @@ class TSPIStore:
 
     def get_average_pos(self):
         record_len = len(self.records)
-        return [self.x_sum/record_len, self.y_sum/record_len, self.z_sum/record_len]
+        return [self.x_total_speed/record_len, self.y_total_speed/record_len, self.z_total_speed/record_len]
